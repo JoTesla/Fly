a9d584d9a00f1bcf0690ebd37cc60118399702b1
Jo <kj_skin@mail.ru>
2020-04-01 16:17:11 +0500

    Попытка новой бранчи

--- app/src/main/java/com/jotesla/attentionfly/settings.java
+++ app/src/main/java/com/jotesla/attentionfly/settings.java
@@ -2,12 +2,15 @@ package com.jotesla.attentionfly;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class settings extends Activity {
     //Settings Vars
     public static class appSettings{
+
+        public boolean start_w_settings;
 
         public static class field {
             public final class row {
@@ -22,6 +25,8 @@ public class settings extends Activity {
     }
 
     protected boolean game_is_running;
+    SharedPreferences preferences;
+    SharedPreferences.Editor prefEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
